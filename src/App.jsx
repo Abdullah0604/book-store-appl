0646bcdbe8de3979acf8d373d545f50cf98620de
greenFruits <mdabdullah6548ctg@gmail.com>
2025-11-04 21:10:48 +0600

    setup: added all necessery images, icons and splited up all single codes into  multiple resuable component

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import BookForm from "./components/BookForm";
+import BookList from "./components/BookList";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <main className="py-12 2xl:px-6">
+        <div className="container grid xl:grid-cols-[auto_350px] 2xl:grid-cols-[auto_400px] gap-4 2xl:gap-8">
+          <BookList />
+
+          <BookForm />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
